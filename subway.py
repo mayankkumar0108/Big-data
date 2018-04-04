be056ee9b3b9eaeed4ca8821862f569ee7da50f7
mayankkumar0108 <mayanksre010895@gmail.com>
2018-04-04 22:03:11 +0530

    Big_data_foundation

---
+++ subway.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  6 23:41:26 2018
+
+@author: mayankkumar
+"""
+
+import pandas as pd
+df=pd.read_csv("turnstile_data_master_with_weather.csv")
+print(df.head())
